032c7015872b9349d7108a87dc2bf00375f1eef2
michaelambroseconway <michaelambroseconway@0525b052-ffcf-11de-82af-0555a752a183>
2010-05-20 04:45:46 +0000

    MC: Perl script _populate_ili.pl_ generates instances and some metadata.  Still need to script relations.

--- demo_onto/ili_sandbox/populate_ili.pl
+++ demo_onto/ili_sandbox/populate_ili.pl
@@ -1,7 +1,11 @@
+# populate_ili.pl May 2010
+# Mike Conway, DBMI Pittsburgh
+
+# Perl script populates the ILI ontology
+# SVN Version Info:
+# $Id$
 
 use RDF::Helper;
-
-
 
 my $rdf = RDF::Helper->new();
 $rdf->include_rdfxml(filename => "ili.owl");
@@ -23,17 +27,41 @@ while (<>) {
     my $symptom    = $field[22];  $symptom = trim($symptom);
     my $sign       = $field[23];  $sign = trim($sign);
 
-    if ($diagnosis == 1) {
+    #lowercase first letter of subconcept
+    $subconcept = lcfirst($subconcept);
+
+    # ADD DIAGNOSIS INSTANCES
+    if (($diagnosis == 1) || ($syndrome == 1)) {
         $rdf->assert_resource("http://www.owl-ontologies.com/Ontology1274288723.owl#$subconcept","http://www.w3.org/1999/02/22-rdf-syntax-ns#type","http://www.owl-ontologies.com/Ontology1274288723.owl#Disease");
     }
+
+    # ADD SIGNS AND PHYSICAL FINDING INSTANCES
+    if (($sign == 1) || ($physical_finding == 1)) {
+       $rdf->assert_resource("http://www.owl-ontologies.com/Ontology1274288723.owl#$subconcept","http://www.w3.org/1999/02/22-rdf-syntax-ns#type","http://www.owl-ontologies.com/Ontology1274288723.owl#Finding");
+   }
+
+    # ADD SYMPTOM INSTANCES
+    if ($symptom == 1) {
+        $rdf->assert_resource("http://www.owl-ontologies.com/Ontology1274288723.owl#$subconcept","http://www.w3.org/1999/02/22-rdf-syntax-ns#type","http://www.owl-ontologies.com/Ontology1274288723.owl#Symptom");
+    }
+
+    # ADD LABEL
+    my $label_subconcept = $subconcept;
+    $label_subconcept =~ s/([A-Z])/ $1/g;
+    $label_subconcept = lc($label_subconcept);
+    $label_subconcept = trim($label_subconcept);
+    my $label_literal = $rdf->new_literal($label_subconcept, "en");
+    $rdf->assert_literal("http://www.owl-ontologies.com/Ontology1274288723.owl#$subconcept","http://www.owl-ontologies.com/Ontology1274288723.owl#label", $label_literal);
+    print "$label_subconcept" . "\n";
     
 }
 
 
 
+$rdf->serialize(filename => 'out.owl', format => 'rdfxml');
 
-sub trim($)
-{
+
+sub trim() {
 	my $string = shift;
 	$string =~ s/^\s+//;
 	$string =~ s/\s+$//;
