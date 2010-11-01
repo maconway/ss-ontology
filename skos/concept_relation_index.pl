c4cc7f32d3a6e51c9fd6eca6f4ac0efd4f600c8b
michaelambroseconway <michaelambroseconway@0525b052-ffcf-11de-82af-0555a752a183>
2010-11-01 04:36:30 +0000

    MC: Additions to SKOS generation script (and indexing scripts)

---
+++ skos/concept_relation_index.pl
@@ -0,0 +1,52 @@
+
+use strict;
+use Spreadsheet::ParseExcel;
+
+my $parser = Spreadsheet::ParseExcel->new();
+my $workbook = $parser->parse("../spreadsheet/ESSO.xls");
+my $worksheet = $workbook->worksheet("concept_data");
+
+my @relation_store;
+my $previous_subconcept;
+my $row_number = 0;
+while ($row_number< 280) {
+    if ($row_number == 0) {$row_number++; next;}
+    
+    my $concept_cell = $worksheet->get_cell($row_number,15);
+    my $concept = $concept_cell->unformatted();
+
+    my $subconcept_cell = $worksheet->get_cell($row_number,16);
+    my $subconcept = $subconcept_cell->unformatted();
+
+    my $relation_cell = $worksheet->get_cell($row_number, 17);
+    my $relation = $relation_cell->unformatted();
+
+    if ($relation =~ /RelatedConcept/) {
+        my $lc_subconcept = lcfirst($subconcept);
+        push(@relation_store, $lc_subconcept);
+        my $lc_previous_subconcept = lcfirst($previous_subconcept);
+        print "$lc_subconcept RELATED_TO $lc_previous_subconcept\n";
+    }
+
+    if ($relation =~ /Synonym/) {
+        my $lc_subconcept = lcfirst($subconcept);
+        push(@relation_store, $lc_subconcept);
+        my $lc_previous_subconcept = lcfirst($previous_subconcept);
+        print "$lc_subconcept RELATED_TO $lc_previous_subconcept\n";
+    }
+
+     if ($relation =~ /ConceptName/) {
+        $previous_subconcept = $subconcept;
+        for(my $i = 0; $i < scalar(@relation_store);$i++) {
+            
+            for (my $j= $i + 1; $j < @relation_store; $j++) {
+                print $relation_store[$i] . " RELATED_TO " . $relation_store[$j] . "\n";
+            }
+        }
+        undef @relation_store;
+    }
+    $row_number++;
+
+}
+    
+    
---
+++ skos/regexp_index.pl
@@ -0,0 +1,55 @@
+use strict;
+use Spreadsheet::ParseExcel;
+use Perl6::Slurp;
+
+my $parser = Spreadsheet::ParseExcel->new();
+my $workbook = $parser->parse("../spreadsheet/ESSO.xls");
+my $worksheet = $workbook->worksheet("concept_data");
+ 
+
+for (my $i = 1; $i < 280; $i++) {
+    my $concept_cell = $worksheet->get_cell($i,16);
+    my $concept = $concept_cell->unformatted();
+    $concept = lcfirst($concept);
+
+    my $keyword_cell = $worksheet->get_cell($i, 34);
+    if (!(defined $keyword_cell)) {next;}
+    my $keyword = $keyword_cell->unformatted();
+    my $keyword = trim($keyword);
+    
+    my @first_split = split(/:::/, $keyword);
+    my @words;
+
+    #deals with vomiting special case
+    ###########################################
+    if ($concept eq "vomiting") {
+        my $vom = slurp("vomit.txt");
+        my @first_split = split(/:::/, $vom);
+        foreach my $triple (@first_split) {
+            my $w = $triple;
+            $w =~ s/^.*?\[(.*?)\].*/$1/g;
+            $w = trim($w);
+            if ($w eq "") { next ;}
+            print ("$concept HAS_REGEXP $w\n");
+        }
+        next;
+    }
+    #############################################
+    foreach my $triple (@first_split) {
+        my $w = $triple;
+        $w =~ s/^.*?\[(.*?)\].*/$1/g;
+        $w = trim($w);
+         print ("$concept HAS_REGEXP $w\n");
+    }
+
+   
+}
+
+
+sub trim {
+    my $string = shift;
+    $string =~ s/^\s+//;
+    $string =~ s/\s+$//;
+    return $string;
+}
+
